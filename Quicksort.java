ba7a85e8e9989799e8c7c83201605510531633aa
Pranit Nagdeote <97742928+Pranit169@users.noreply.github.com>
2022-11-30 08:43:35 +0530

    Update Quicksort.java

--- Quicksort.java
+++ Quicksort.java
@@ -58,6 +58,7 @@ class Quicksort{
         int n=arr.length; 
         System.out.println("Array before sorting: ");
         printArray(arr);
+        System.out.println("Array after sorting: ");
         quickSort(arr,0,n-1);
         printArray(arr);
     }
