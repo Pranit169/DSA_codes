29e1fc3e2a17d2fc614322c6795c5cd8e4a3fd2a
Pranit <pranitnagdeote89@gmail.com>
2022-12-22 20:23:11 +0530

    git push

--- Mergesort.java
+++ Mergesort.java
@@ -1,6 +1,25 @@
 import java.util.Scanner;
 
 public class Mergesort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements for array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array before sorting: ");
+        printArray(arr);
+
+        System.out.println("\nArray after sorting: ");
+        mergeSort(arr, 0, n - 1);
+
+        printArray(arr);
+    }
     // To print the sorted array.
     public static void printArray(int a[]) {
         for (int i = 0; i < a.length; i++) {
@@ -59,23 +78,5 @@ public class Mergesort {
         merge(a, start, end);
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the length of array: ");
-        int n = sc.nextInt();
-
-        int[] arr = new int[n];
-        System.out.println("Enter the elements for array: ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        System.out.println("Array before sorting: ");
-        printArray(arr);
-
-        System.out.println("\nArray after sorting: ");
-        mergeSort(arr, 0, n - 1);
-
-        printArray(arr);
-    }
+    
 }
