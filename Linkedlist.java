2adb1297ef143ee9778cf8554bc64aa581fb9efb
Pranit <pranitnagdeote89@gmail.com>
2022-12-26 12:30:57 +0530

    git push

--- Linkedlist.java
+++ Linkedlist.java
@@ -10,7 +10,10 @@ class Linkedlist {
 			this.next = null;
 		}
 	}
+public void atanyposition(){
+	System.out.println("HI");
 
+}
 	public void addAtPosition(String data, int position) {
 		if (position == 1) {
 			addFirst(data);
