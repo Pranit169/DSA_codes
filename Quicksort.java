27db74395de15a27626d73fe95cc734d1d65e544
Pranit <pranitnagdeote89@gmail.com>
2023-02-04 16:33:57 +0530

    git pull

--- Quicksort.java
+++ Quicksort.java
@@ -1,61 +1,32 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 class Quicksort {
-    // To print the sorted array.
-    public static void printArray(int a[]) {
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i] + " ");
-        }
-    }
-
-    public static int partition(int a[], int start, int end) {
-        int temp1, temp2;
-        int pivot = a[start];
-        int count = 0;
-        // To count the position of pivot.
-        for (int i = start + 1; i <= end; i++) {
-            if (pivot >= a[i]) {
-                count++;
-            }
-        }
-        int pivotIndex = start + count;
-        // Swapping element of pivotIndex and start
-        temp1 = a[pivotIndex];
-        a[pivotIndex] = a[start];
-        a[start] = temp1;
-
-        int i = start, j = end;
-        // Traversing the array
-        while (i < pivotIndex && j > pivotIndex) {
-            while (a[i] <= pivot) {
-                i++;
-            }
-            while (a[j] > pivot) {
-                j--;
-            }
-            // Swapping the elements
-            if (i < pivotIndex && j > pivotIndex) {
-                temp2 = a[i];
-                a[i] = a[j];
-                a[j] = temp2;
-                i++;
-                j--;
-            }
-        }
-        return pivotIndex;
-
-    }
-
-    public static void quickSort(int a[], int start, int end) {
-        if (start >= end) {
+    public static void quickSort(int a[], int low, int high) {
+        if (low >= high) {
             return;
         }
-
-        int p = partition(a, start, end);
-        // To sort numbers less than piviot
-        quickSort(a, start, p - 1);
-        // To sort numbers greater than piviot
-        quickSort(a, p + 1, end);
+        int start = low;
+        int end = high;
+        int mid = start + (end - start) / 2;
+        int pivot = a[mid];
+        while (start <= end) {
+            while (a[start] < pivot) {
+                start++;
+            }
+            while (a[end] > pivot) {
+                end--;
+            }
+            if (start <= end) {
+                int temp = a[start];
+                a[start] = a[end];
+                a[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        quickSort(a, low, end);
+        quickSort(a, start, high);
     }
 
     public static void main(String[] args) {
@@ -70,11 +41,11 @@ class Quicksort {
         }
 
         System.out.println("Array before sorting: ");
-        printArray(arr);
+        System.out.println(Arrays.toString(arr));
 
         System.out.println("\nArray after sorting: ");
         quickSort(arr, 0, n - 1);
+        System.out.println(Arrays.toString(arr));
 
-        printArray(arr);
     }
 }
